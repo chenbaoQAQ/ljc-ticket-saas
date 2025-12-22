ef85e9c2d5931550bdcb7671e7531ab8ab167bff
chenbaoQAQ <junchenli180@gmail.com>
2025-12-22 09:29:02 +0800

    feat: 导入 Spring Boot 初始结构

---
+++ src/main/java/com/example/ljcticketsaas/LjcTicketSaasApplication.java
@@ -0,0 +1,13 @@
+package com.example.ljcticketsaas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LjcTicketSaasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LjcTicketSaasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ljcticketsaas/LjcTicketSaasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ljcticketsaas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LjcTicketSaasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
